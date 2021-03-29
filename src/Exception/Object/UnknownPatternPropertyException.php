876bea9d7a7e9256140e9009290cf0fe43405a69
Enno Woortmann <enno.woortmann@web.de>
2021-03-29 15:12:42 +0200

    Add UnknownPatternPropertyException

---
+++ src/Exception/Object/UnknownPatternPropertyException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPModelGenerator\Exception\Object;
+
+use Exception;
+
+/**
+ * Class UnknownPatternPropertyException
+ *
+ * @package PHPModelGenerator\Exception\Object
+ */
+class UnknownPatternPropertyException extends Exception
+{
+}
