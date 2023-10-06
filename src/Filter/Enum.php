4f912db7268596b17646ac32666d7f2257d1d657
Enno Woortmann <enno.woortmann@web.de>
2023-10-06 14:57:40 +0200

    Allow non-backed enums in enum filter

--- src/Filter/Enum.php
+++ src/Filter/Enum.php
@@ -4,16 +4,16 @@ declare(strict_types = 1);
 
 namespace PHPModelGenerator\Filter;
 
-use BackedEnum;
+use UnitEnum;
 
 class Enum
 {
-    public static function filter($value, $options): ?BackedEnum
+    public static function filter($value, $options): ?UnitEnum
     {
         return $value === null ? null : $options['fqcn']::from($value);
     }
 
-    public static function serialize(?BackedEnum $value): null | int | string
+    public static function serialize(?UnitEnum $value): null | int | string
     {
         return $value?->value;
     }
